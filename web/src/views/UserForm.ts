6e7416c7071290a245a2f02862d12de754d7df9e
Weber <weberchen85@gmail.com>
2020-12-06 00:47:53 -0600

    complete change name

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -14,11 +14,18 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
     };
   }
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
+  };
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+    const name = input.value;
+    this.model.set({ name });
   };
 
   template(): string {
@@ -29,7 +36,7 @@ export class UserForm {
       <div>User age: ${this.model.get('age')}</div>
 
       <input/>
-      <button>Click me</button>
+      <button class="set-name">Change Name</button>
       <button class="set-age">Set Random Age</button>
     </div>
     `;
